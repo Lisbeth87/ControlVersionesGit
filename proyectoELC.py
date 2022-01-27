45bad44912a0012fad3dc0e1f983df30507045ee
Esther <cuenta.lisbeth@gmail.com>
2022-01-27 12:01:41 +0100

    Importacion de librerias pandas y numpy

---
+++ proyectoELC.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 27 11:58:46 2022
+
+@author: EstherLanchaCañas
+"""
+
+#============================================= IMPORTACIÓN DE LAS LIBRERÍAS =============================================#
+
+import pandas as pd
+import numpy as np
